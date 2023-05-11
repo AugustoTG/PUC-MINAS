990abc11e551c7adfb9effd54d091fac460dbda5
AugustoTG <121759913+AugustoTG@users.noreply.github.com>
2023-05-11 10:40:40 -0300

    Update index.js

--- 02 - Linguagem Java Script (2023)/ex 01/index.js
+++ 02 - Linguagem Java Script (2023)/ex 01/index.js
@@ -1,12 +1,19 @@
 const backgroud = document.querySelector('body');
+const text = document.querySelector('#text');
+const hour = document.querySelector('#hour')
 const execute = ()=>{
+    backgroud.style.background = "#fff"
     alert('Hello Word');
 }
 
 const changeColor = ()=>{
-    const text = document.querySelector('#text');
     text.addEventListener('mouseover', ()=> {backgroud.style.background = "#fff655"});
     text.addEventListener('mouseout', ()=> {backgroud.style.background = "green"});
 }
 changeColor()
 
+
+(function(){
+    hour = new Data();
+
+})()

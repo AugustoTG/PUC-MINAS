2cc8930dc7569d5619632a0cef50810f72a355ad
AugustoTG <121759913+AugustoTG@users.noreply.github.com>
2023-06-09 21:09:35 -0300

    Update index.js

--- 02 - Linguagem Java Script (2023)/2.6 form/index.js
+++ 02 - Linguagem Java Script (2023)/2.6 form/index.js
@@ -1,12 +1,13 @@
 const login = document.getElementById('login')
 const nome = document.getElementById('name')
 const password = document.getElementById('senha')
+const idade = document.getElementById('idade')
 onload = ()=>{
     login.onsubmit = () => {
         nome.onchange = () =>{
-            console.log(nome)
+            console.log(nome.value)
         }
-       if(nome.value === '' || password.value === ''){
+       if(nome.value === '' || password.value === '' || idade.value === ''){
             alert('preencha todos os campos')
             return false;
        }

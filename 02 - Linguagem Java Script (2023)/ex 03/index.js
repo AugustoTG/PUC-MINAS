6b7055f53234f1d4cc76e712f216fb93634ac110
AugustoTG <121759913+AugustoTG@users.noreply.github.com>
2023-06-21 20:16:12 -0300

    Update index.js

--- 02 - Linguagem Java Script (2023)/ex 03/index.js
+++ 02 - Linguagem Java Script (2023)/ex 03/index.js
@@ -1,2 +1,25 @@
 
 const sliderShow = document.querySelector('.sliderShow')
+
+const img = [
+    'img/biel-morro-kcKiBcDTJt4-unsplash.jpg',
+    'img/f1.jpg',
+    'img/f2.jpg',
+    'img/f3.jpg',
+    'img/f4.jpg',
+    'img/f5.jpg',
+    'img/f6.jpg',
+]
+const time = 500;
+const changeImg = ()=>{
+    
+    const i = 0;
+    sliderShow.setAttribute("src", img[i]);
+    if(i < img.length - 1){
+        i++
+    }else{
+        i=0;
+    }      
+    setInterval(changeImg(), time)
+}
+window.onload = changeImg

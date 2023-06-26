b7045b10e76aa98b3cec95a8f3fc5b476183aaf5
AugustoTG <121759913+AugustoTG@users.noreply.github.com>
2023-06-26 12:00:47 -0300

    Update index.js

--- 02 - Linguagem Java Script (2023)/4.3 IndexdBD/index.js
+++ 02 - Linguagem Java Script (2023)/4.3 IndexdBD/index.js
@@ -1,2 +1,24 @@
 
-let BD
+let bd;
+let reqBD = indexedDB.open('meuBD', 1);
+
+const livros = [
+    {id: 1, titulo: 'Ilída', autor: 'Homero'},
+    {id: 2, titulo: 'Odisseia', autor: 'Homero'}
+]
+
+reqBD.onsuccess = () => {
+    bd = reqBD.result;
+};
+
+reqBD.onerror = ()=>{
+console.log(reqBD.error);
+
+};
+
+reqBD.onupgradeneeded = (e) =>{
+    bd = reqBD.result;
+    if(!bd.objectStoreNames.contains('livros')){
+        bd.createObjectStore('livros', {keyPath: 'id'})
+    }
+}

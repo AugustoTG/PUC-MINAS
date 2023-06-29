977ab7cbfd1923236f07b274aa566a38d8ae3ed0
AugustoTG <121759913+AugustoTG@users.noreply.github.com>
2023-06-29 17:42:27 -0300

    Update index.js

--- 02 - Linguagem Java Script (2023)/4.6 file/index.js
+++ 02 - Linguagem Java Script (2023)/4.6 file/index.js
@@ -1,6 +1,7 @@
 const arquivos = document.querySelector('#file')
 const btnArquivos = document.querySelector('#btnArquivos')
 const arq = document.querySelector('#arq')
+const arquivoTexto = document.querySelector('#arquivoTexto')
 onload = () =>{
     
     arquivos.onchange = () => {
@@ -9,8 +10,21 @@ onload = () =>{
         //     arq.innerHTML += arquivos.files[i].name + '<br/>'
         //     console.log(arquivos.files[i])
         // }
+        let fr = new FileReader();
+        fr.readAsDataURL(arquivos.files[0]);
+        fr.onload = (e) => {
+            arq.innerHTML = e.target.result;
+        }
     }
     btnArquivos.onclick = () => {
         arquivos.click()
+    }
+
+    arquivoTexto.onchange = () => {
+        let fr = new FileReader();
+        fr.readAsText(arquivoTexto.files[0], 'UTF-8');
+        fr.onload = (e) => {
+            arq.innerHTML = e.target.result;
+        }
     }
 }
